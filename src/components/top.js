179c50d01d4c64788b07998de927a5900a5b6129
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2022-04-23 02:58:20 -0600

    coming soon

---
+++ src/components/top.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class head extends Component {
+  render() {
+    return (
+      <div className='Top'>
+        <h1>Erick Domínguez</h1>
+        <h2>portfolio coming soon</h2>
+        </div>
+    )
+  }
+}
